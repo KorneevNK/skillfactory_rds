161c39d82c6851ff2ebfaac82b83038f46c57e3d
KorneevNK <korneevnk@ya.ru>
2021-03-25 00:53:21 +0300

    Update numbers.py

--- module_0/numbers.py
+++ module_0/numbers.py
@@ -1,34 +1,36 @@
 import numpy as np
+from random import randint
 count = 0                            # счетчик попыток
 number = np.random.randint(1,100)    # загадали число
 print ("Загадано число от 1 до 99")
 
-def score_game(game_core_v1):
+def score_game(game_core):
     '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
     count_ls = []
     np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
     random_array = np.random.randint(1, 101, size=(1000))
     for number in random_array:
-        count_ls.append(game_core_v1(number))
+        count_ls.append(game_core(number))
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
     return(score)
 
 def game_core_v2(number):
-    '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
-       Функция принимает загаданное число и возвращает число попыток'''
+    '''С каждой попыткой отыскиваем верхнюю и нижнюю границы для генерации случайного числа в этом диапазоне'''
     count = 0
     predict = 50
+    low = 1
+    high = 100
+
     while number != predict:
         count+=1
-        if number - predict >10:
-            predict +=10
-        elif predict - number >10:
-            predict -=10
-        elif number > predict: 
-            predict += 2
-        elif number < predict: 
-            predict -= 3
+        if predict>number:
+        	high=predict
+        else:
+        	low=predict + 1
+
+        predict=randint(low,high)
+
     return(count) # выход из цикла, если угадали
 
 # Проверяем
